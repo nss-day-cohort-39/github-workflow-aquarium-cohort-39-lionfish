1acf88b90321379e647f92e448964de9c44d0f01
buckleyderekp <buckleyderekp@gmail.com>
2020-02-24 10:47:44 -0600

    added lionfish to fish list and dialog button

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -49,6 +49,14 @@ const initializeDetailButtonEvents = () => {
             theDialog.showModal()
         }
     )
+    // Show Julius's details when the button is clicked
+    document.querySelector("#button--julius").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--julius")
+            theDialog.showModal()
+        }
+    )
 }
 
 export default initializeDetailButtonEvents
