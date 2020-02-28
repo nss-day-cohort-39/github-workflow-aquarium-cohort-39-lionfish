6247457030a6fb547cbd2c5fcbc635e03407bd7a
ianpenn08 <inkmpenn@gmail.com>
2020-02-28 10:48:29 -0600

    added tipDataProvider and other JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
   
 import initializeDetailButtonEvents from './dialog.js'
-import { useFish } from './FishDataProvider.js'
-import FishList from './FishList.js'
+import { useFish } from './fish/FishDataProvider.js'
+import FishList from './fish/FishList.js'
+import TipList from './tips/TipList.js'
+TipList()
 
 // initializeDetailButtonEvents()
 
---
+++ scripts/tips/Tip.js
@@ -0,0 +1,9 @@
+// made variable 
+const Tips = (tips) => {
+    return `
+<section class="tip">${tips.tip}</section>
+`
+
+}
+
+export default Tips
